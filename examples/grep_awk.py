54ef35470664e65acb60b6b3e4522476438d05ae
burrutia <burrutia@oakley.com>
2011-07-30 00:07:14 -0700

    Edited examples/grep_awk.py via GitHub

--- examples/grep_awk.py
+++ examples/grep_awk.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python
 # example usage for unix_util module
 
-import os, sys, string
-sys.path.append("/home/emu/modules")
-import unix_util
+import os, sys, string, re
+sys.path.append("/home/control/modules")
 from unix_util import shell_utils
+from ec2_cluster import conn
 
 a = shell_utils()
-
-object = "my_secgroup  10.220.10.123 dev t1.large"
-pattern = 'dev'
-awkval = int('1')
-a.awk(object=object,smatch=pattern,ph=awkval)
+rstatus = 'running'
+e = len(conn.get_all_instances())
+rs = conn.get_all_instances()
+for x in range(e):
+    r = rs[x]
+    s = r.groups[0]
+    i = r.instances[0]
+    if rstatus in i.state:
+        position = int(1)
+        unicode_dnsname = i.private_dns_name
+        string_dnsname = str(unicode_dnsname)
+        ipaddr_tmp = string_dnsname.replace('-', '.')
+        ip_match = re.compile('[0-9]{2}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}')
+        ip_string = ip_match.findall(ipaddr_tmp)
+        ip = ip_string[0]
+        fqdn = a.host(object=i.state, ip=ip)
+        shorthostname = a.cut(object=fqdn, delimeter='.', position=position)
+        print "%s %s %s %s %s %s" %(s.id, i.dns_name, i.private_dns_name, ip, shorthostname, i.state)
+    if not rstatus in i.state:
+        print "%s %s %s %s " %(s.id, i.dns_name, i.private_dns_name, i.state)
