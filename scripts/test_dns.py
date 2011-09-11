acc759973a7ee11351e5ec3d97aa79fb95646ca0
root <root@aws1prdns1.burrutiaprd.internal>
2011-09-11 13:03:12 -0400

    Added example script to verify dns module

---
+++ scripts/test_dns.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+# Copyright (c) 2011 Brian Urrutia <burrutia.biz@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, dis-
+# tribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the fol-
+# lowing conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
+# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+from  commands import getoutput
+import os, re, string, ConfigParser
+import sys
+from shutil import *
+import optparse
+import fileinput
+import IPy
+from IPy import IP
+import subprocess
+
+config_file ='/etc/datacenter.ini'
+
+config = ConfigParser.ConfigParser()
+config.readfp(open(config_file))
+
+setupdir = config.get('master-conf','setup')
+basedir = config.get('master-conf','basedir')
+bizunit = config.get('master-conf','unit')
+env = config.get('master-conf','env')
+
+mdir = ('%s/modules' %(basedir))
+
+sys.path.append(mdir)
+
+domain = bizunit + env
+
+from dns import EC2_Dns
+
+def controller():
+    global VERBOSE
+    p = optparse.OptionParser()
+    global options
+    global thostname
+    global ipaddr
+    p.add_option('--ipaddr','-i', action="store", type="string", help="ip address")
+    p.add_option('--thostname','-H', action="store", type="string", help="target hostname")
+    p.add_option('--verbose', '-v', action = 'store_true', help='prints verbosely', default=False)
+
+    options, arguments = p.parse_args()
+    if options.verbose:
+        VERBOSE=True
+    if options.ipaddr:
+        VERBOSE=False
+    else:
+        p.print_help()
+
+def main():
+    controller()
+
+if __name__ == '__main__':
+    main()
+
+ipaddr = options.ipaddr
+thostname = options.thostname
+
+dns = EC2_Dns()
+# will change soon as one call
+drev = dns.rev_ip(ipaddr)
+da = dns.a_seg(ipaddr)
+dpre = dns.p_zone()
+dtout = dns.t_out()
+g = dns.get_all_zfname(domain, thostname)
+dns.write_ptr_record(dpre, da, ipaddr, drev, dtout, thostname, domain )
+dns.write_zone_entry(drev )
+dns.write_a_record(dpre, da, ipaddr, drev, dtout, thostname)
+dns.clean_files()
